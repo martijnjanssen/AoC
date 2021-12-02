4368e7d8c3c56b3751c503b6c6589f1d185f0464
Martijn Janssen <martijn9612+github@gmail.com>
2021-12-02 10:29:44 +0100

    [2021d1] Cleanup code

--- 2021/day_1/main.go
+++ 2021/day_1/main.go
@@ -1,31 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/martijnjanssen/aoc/pkg/helper"
-
-	"github.com/sirupsen/logrus"
 )
 
+// Middle of the window doesn't have to be considered,
+// the middle of the window is the same for both instances.
 func main() {
 	ds := []int{}
 
 	helper.DownloadAndRead(1, func(l string) {
-		v, err := strconv.Atoi(l)
-		if err != nil {
-			logrus.Fatalf("Unable to read line: %s", err)
-		}
-
+		v, _ := strconv.Atoi(l)
 		ds = append(ds, v)
 	})
 
 	inc := 0
 	for i := 1; i < len(ds)-2; i++ {
-		if ds[i-1]+ds[i]+ds[i+1] < ds[i]+ds[i+1]+ds[i+2] {
+		if ds[i-1] < ds[i+2] {
 			inc++
 		}
 	}
 
-	logrus.Infof("Increased is: %d", inc)
+	fmt.Printf("Increased is: %d\n", inc)
 }
