535ce44b8aad7c267f2a5bc316e9cd1d7d4cab03
Martijn Janssen <martijn9612+github@gmail.com>
2021-12-08 10:29:05 +0100

    [2021d8] Solve both parts

---
+++ 2021/day_8/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+	"strings"
+
+	"github.com/martijnjanssen/aoc/pkg/helper"
+)
+
+var numbers map[string]int
+var rNumbers map[int]string
+
+func main() {
+	occurrences := 0
+	acc := 0
+
+	helper.DownloadAndRead(8, func(l string) {
+		numbers = map[string]int{}
+		rNumbers = map[int]string{}
+		lSplit := strings.Split(l, "|")
+		iSplit := strings.Split(strings.TrimSpace(lSplit[0]), " ")
+		oSplit := strings.Split(strings.TrimSpace(lSplit[1]), " ")
+		// Sort such that the 6 comes before the 5,
+		// length 6 strings should be figured out first.
+		sort.Slice(iSplit, func(i, j int) bool {
+			if len(iSplit[i]) == 5 {
+				return false
+			}
+			if len(iSplit[j]) == 5 {
+				return true
+			}
+			return len(iSplit[i]) < len(iSplit[j])
+		})
+
+		for _, s := range iSplit {
+			s = sortString(s)
+			switch len(s) {
+			case 2:
+				storeNums(1, s)
+			case 3:
+				storeNums(7, s)
+			case 4:
+				storeNums(4, s)
+			case 7:
+				storeNums(8, s)
+			case 6:
+				if hasActivatedSegments(s, rNumbers[4]) {
+					storeNums(9, s)
+				} else if hasActivatedSegments(s, rNumbers[1]) {
+					storeNums(0, s)
+				} else {
+					storeNums(6, s)
+				}
+			case 5:
+				if hasActivatedSegments(s, rNumbers[1]) {
+					storeNums(3, s)
+				} else if hasActivatedSegments(rNumbers[6], s) {
+					storeNums(5, s)
+				} else {
+					storeNums(2, s)
+				}
+			}
+		}
+
+		for i, s := range oSplit {
+			// Calculation for part 1
+			if len(s) == 7 || len(s) == 4 || len(s) == 3 || len(s) == 2 {
+				occurrences++
+			}
+
+			// Results part 2
+			acc += numbers[sortString(oSplit[i])] * int(math.Pow10(3-i))
+		}
+	})
+
+	fmt.Printf("Answer is: %d\n", occurrences)
+	fmt.Printf("Answer is: %d\n", acc)
+}
+
+func storeNums(n int, s string) {
+	numbers[s] = n
+	rNumbers[n] = s
+}
+
+func sortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+}
+
+func hasActivatedSegments(c string, h string) bool {
+	for _, l := range h {
+		if strings.IndexRune(c, l) == -1 {
+			return false
+		}
+	}
+	return true
+}
