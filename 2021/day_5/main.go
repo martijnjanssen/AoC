e515d5d0f79755ad9136007a490644f062f5e184
Martijn Janssen <martijn9612+github@gmail.com>
2021-12-17 21:15:32 +0100

    [2021d5] Rewrite without needless struct creation

--- 2021/day_5/main.go
+++ 2021/day_5/main.go
@@ -8,52 +8,48 @@ import (
 	"github.com/martijnjanssen/aoc/pkg/runner"
 )
 
-type point struct {
-	x int
-	y int
-}
-
 type line struct {
-	a *point
-	b *point
+	ay int
+	ax int
+	by int
+	bx int
 }
 
-func makeRange(a, b int) []int {
-	c := 1
-	if a > b {
-		c = -1
+func plotStraightLine(grid [][]int, l *line) {
+	if l.ay == l.by {
+		if l.ax > l.bx {
+			l.ax, l.bx = l.bx, l.ax
+		}
+		for i := l.ax; i <= l.bx; i++ {
+			grid[l.ay][i]++
+		}
+	} else if l.ax == l.bx {
+		if l.ay > l.by {
+			l.ay, l.by = l.by, l.ay
+		}
+		for i := l.ay; i <= l.by; i++ {
+			grid[i][l.ax]++
+		}
 	}
-	r := make([]int, abs(a-b)+1)
-	for i := range r {
-		r[i] = a + c*i
-	}
-	return r
 }
 
-func (l *line) getPoints() []int {
-	ps := []int{}
-	if l.a.y == l.b.y {
-		for _, i := range makeRange(l.a.x, l.b.x) {
-			ps = append(ps, i, l.a.y)
-		}
-	} else if l.a.x == l.b.x {
-		for _, i := range makeRange(l.a.y, l.b.y) {
-			ps = append(ps, l.a.x, i)
-		}
+func plotDiagonalLine(grid [][]int, l *line) {
+	if l.ay == l.by || l.ax == l.bx {
+		return
 	}
-	return ps
-}
 
-func (l *line) getDiagonalPoints() []int {
-	ps := []int{}
-	if l.a.y != l.b.y && l.a.x != l.b.x {
-		ys := makeRange(l.a.y, l.b.y)
-		xs := makeRange(l.a.x, l.b.x)
-		for i := range ys {
-			ps = append(ps, xs[i], ys[i])
-		}
+	if l.ay > l.by {
+		l.ay, l.ax, l.by, l.bx = l.by, l.bx, l.ay, l.ax
 	}
-	return ps
+	d := 1
+	if l.ax > l.bx {
+		d = -1
+	}
+
+	for i := l.ay; i <= l.by; i++ {
+		grid[i][l.ax]++
+		l.ax += d
+	}
 }
 
 type run struct{}
@@ -68,8 +64,8 @@ func (r *run) Run() (a int, b int) {
 
 	helper.DownloadAndRead(5, func(l string) {
 		ps := parseLine(l)
-		lines = append(lines, &line{&point{ps[0], ps[1]}, &point{ps[2], ps[3]}})
-		if m := max(max(ps[0], ps[1]), max(ps[2], ps[3])); m > maxV {
+		lines = append(lines, &line{ps[0], ps[1], ps[2], ps[3]})
+		if m := helper.Max(helper.Max(ps[0], ps[1]), helper.Max(ps[2], ps[3])); m > maxV {
 			maxV = m
 		}
 	})
@@ -81,19 +77,13 @@ func (r *run) Run() (a int, b int) {
 	}
 
 	// Write add non-diagonal lines to grid
-	for _, l := range lines {
-		points := l.getPoints()
-		for i := 0; i < len(points)/2; i++ {
-			grid[points[i*2]][points[i*2+1]] += 1
-		}
+	for i := range lines {
+		plotStraightLine(grid, lines[i])
 	}
 	a = calculateCriticals(grid)
 
-	for _, l := range lines {
-		points := l.getDiagonalPoints()
-		for i := 0; i < len(points)/2; i++ {
-			grid[points[i*2]][points[i*2+1]] += 1
-		}
+	for i := range lines {
+		plotDiagonalLine(grid, lines[i])
 	}
 	b = calculateCriticals(grid)
 	return
@@ -121,18 +111,4 @@ func parseLine(l string) []int {
 		r = append(r, i)
 	}
 	return r
-}
-
-func max(x, y int) int {
-	if x < y {
-		return y
-	}
-	return x
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -1 * x
-	}
-	return x
 }
