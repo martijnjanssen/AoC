e32c7ee2db5695b096a758d28bd47f1730d74c88
Martijn Janssen <martijn9612+github@gmail.com>
2021-12-21 20:30:19 +0100

    [2021d21] Solve part 2

--- 2021/day_21/main.go
+++ 2021/day_21/main.go
@@ -1,7 +1,6 @@
 package day_21
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 
@@ -21,19 +20,26 @@ type player struct {
 }
 
 var (
-	rolls = 0
-	roll  = 1
+	rolls int
+	roll  int
+	store map[[4]int][2]int
 )
 
 func (r *run) Run() (a int, b int) {
 	ps := [2]player{}
 	psi := 0
+	rolls = 0
+	roll = 1
+	store = map[[4]int][2]int{}
 	helper.DownloadAndRead(21, func(l string) {
 		s := strings.Split(l, "starting position: ")
 		n, _ := strconv.Atoi(s[1])
-		ps[psi] = player{position: n}
+		ps[psi] = player{position: n - 1}
 		psi++
 	})
+
+	// calculate part 2 first otherwise starting positions are modified
+	b = helper.Max(play(ps[0].position, ps[1].position, 0, 0))
 
 game:
 	for {
@@ -47,13 +53,9 @@ game:
 				}
 			}
 			rolls += 3
-			p.position = (p.position+r-1)%10 + 1
-			p.score += p.position
-			fmt.Printf("player %d (score: %d): rolled %d, is in %d\n", i, p.score, r, p.position)
-
+			p.position = (p.position + r) % 10
+			p.score += p.position + 1
 			if p.score >= 1000 {
-				fmt.Println(ps[1-i].score)
-				fmt.Println(rolls)
 				a = ps[1-i].score * rolls
 				break game
 			}
@@ -61,4 +63,33 @@ game:
 	}
 
 	return
+}
+
+func play(p1 int, p2 int, s1 int, s2 int) (int, int) {
+	if s1 >= 21 {
+		return 1, 0
+	}
+	if s2 >= 21 {
+		return 0, 1
+	}
+	storeArr := [4]int{p1, p2, s1, s2}
+	if res, ok := store[storeArr]; ok {
+		return res[0], res[1]
+	}
+	w1, w2 := 0, 0
+	for d1 := 1; d1 <= 3; d1++ {
+		for d2 := 1; d2 <= 3; d2++ {
+			for d3 := 1; d3 <= 3; d3++ {
+				new_p1 := (p1 + d1 + d2 + d3) % 10
+				new_s1 := s1 + new_p1 + 1
+
+				wp2, wp1 := play(p2, new_p1, s2, new_s1)
+				w1 += wp1
+				w2 += wp2
+			}
+		}
+	}
+
+	store[storeArr] = [2]int{w1, w2}
+	return w1, w2
 }
