d7b7cbb19334c31ad9154309826239c680f0aa76
Martijn Janssen <martijn9612+github@gmail.com>
2025-12-03 23:04:22 +0100

    [2025d2] solve part 2

--- 2025/day2.c
+++ 2025/day2.c
@@ -20,7 +20,8 @@ int main(void) {
        return 1;
     }
 
-    uint64_t invalids = 0;
+    uint64_t invalids_a = 0;
+    uint64_t invalids_b = 0;
 
     char *pair = strtok(line, ",");
     uint64_t a, b;
@@ -32,27 +33,55 @@ int main(void) {
             continue;
         }
 
-        char str[50]="", x[25]="", y[25]="";
         int str_len;
+        // Size of the array for the string to fit based on the highest number in the pair
+        int malloc_len = (int)(log10(b)+1);
+        char *str = (char*) malloc(malloc_len);
+        char *x = (char*) malloc(malloc_len/2);
+        char *y = (char*) malloc(malloc_len/2);
 
+        // For each between the lower and upper bound
         for (uint64_t i = a; i <= b; i++) {
+            // Create the string from integer
             sprintf(str, "%" PRIu64, i);
-            str_len = strlen(str) - 1;
+            str_len = strlen(str);
 
-            if (str_len % 2 == 0) {
-                continue;
-            }
+            // For each length less than half of the string length
+            for (int l = str_len/2; l >= 1; l--) {
+                // If remainder is non-zero
+                if (str_len % l != 0) {
+                    continue;
+                }
 
-            strncpy(x, str, str_len/2+1);
-            strncpy(y, str + (str_len/2+1), str_len);
+                // reset values, string buffers
+                memset(x,0,strlen(x));
+                memset(y,0,strlen(y));
+                int equal = 1;
 
-            if (strcmp(x, y) == 0) {
-                invalids += i;
+                // Compare all subsequent offsets with the first portion
+                strncpy(x, str, l);
+                for (int ofst = 0; ofst <= str_len - l; ofst += l) {
+                    strncpy(y, str + ofst, l);
+
+                    if (strcmp(x, y) != 0) {
+                        equal = 0;
+                    }
+                }
+
+                // If all offsets are the same
+                if (equal > 0) {
+                    invalids_b += i;
+                    // If there are only two offsets
+                    if (l == str_len/2 && str_len % 2 == 0) {
+                       invalids_a += i;
+                    }
+                    break;
+                }
             }
         }
 
         pair = strtok(NULL, ",");
     }
 
-    printf("Invalids: %" SCNu64 "\n", invalids);
+    printf("a: %" PRIu64 ", b: %" PRIu64 "\n", invalids_a, invalids_b);
 }
